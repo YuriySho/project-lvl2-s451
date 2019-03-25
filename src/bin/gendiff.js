7144274c1e943ba1fc7c0427790b429cbcdc3443
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-03-25 15:23:30 +0300

    version 1.0.1

---
+++ src/bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import { version } from '../../package.json';
+
+program
+  .version(version)
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
