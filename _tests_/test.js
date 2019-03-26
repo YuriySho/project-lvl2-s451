e80ed1ed8818161fe3eea3b06a6d4d0155ea187b
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-03-26 18:06:02 +0300

    add new test for yaml

--- _tests_/test.js
+++ _tests_/test.js
@@ -4,6 +4,13 @@ import genDiff from '../src';
 const resultPath = '_tests_/_fixtures_/result.txt';
 const result = fs.readFileSync(resultPath, 'utf8');
 
-test('test', () => {
+test('test json', () => {
+  const beforePath = '_tests_/_fixtures_/before.json';
+  const afterPath = '_tests_/_fixtures_/after.json';
+  expect(genDiff(beforePath, afterPath)).toBe(result);
+});
+test('test yaml', () => {
+  const beforePath = '_tests_/_fixtures_/before.yml';
+  const afterPath = '_tests_/_fixtures_/after.yml';
   expect(genDiff(beforePath, afterPath)).toBe(result);
 });
