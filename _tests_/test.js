c83f02effacb3a96c4f8f73b9b3a550f4f0e8ca8
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-03-26 23:34:28 +0300

    add .ini test and add test.each

--- _tests_/test.js
+++ _tests_/test.js
@@ -3,14 +3,17 @@ import genDiff from '../src';
 
 const resultPath = '_tests_/_fixtures_/result.txt';
 const result = fs.readFileSync(resultPath, 'utf8');
+const beforePathJson = '_tests_/_fixtures_/before.json';
+const afterPathJson = '_tests_/_fixtures_/after.json';
+const beforePathYml = '_tests_/_fixtures_/before.yml';
+const afterPathJYml = '_tests_/_fixtures_/after.yml';
+const beforePathIni = '_tests_/_fixtures_/before.ini';
+const afterPathIni = '_tests_/_fixtures_/after.ini';
 
-test('test json', () => {
-  const beforePath = '_tests_/_fixtures_/before.json';
-  const afterPath = '_tests_/_fixtures_/after.json';
-  expect(genDiff(beforePath, afterPath)).toBe(result);
-});
-test('test yaml', () => {
-  const beforePath = '_tests_/_fixtures_/before.yml';
-  const afterPath = '_tests_/_fixtures_/after.yml';
-  expect(genDiff(beforePath, afterPath)).toBe(result);
-});
+test.each([[beforePathJson, afterPathJson], [beforePathYml, afterPathJYml],
+  [beforePathIni, afterPathIni]])(
+  'get difference',
+  (beforePath, afterPath) => {
+    expect(genDiff(beforePath, afterPath)).toBe(result);
+  },
+);
