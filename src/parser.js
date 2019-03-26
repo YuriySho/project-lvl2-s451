ed44e6c7145d6ed3d60c0397f1e25522320b264b
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-03-26 23:35:24 +0300

    add .ini parse

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parse = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.decode,
 };
 
 export default (data, format) => parse[format](data);
