3a0dca815c2d5cf35261de871db75a817563dd0e
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-03-26 18:05:05 +0300

    v1.3.1

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const beforePath = '_tests_/_fixtures_/before.json';
-const afterPath = '_tests_/_fixtures_/after.json';
 const resultPath = '_tests_/_fixtures_/result.txt';
 const result = fs.readFileSync(resultPath, 'utf8');
 
